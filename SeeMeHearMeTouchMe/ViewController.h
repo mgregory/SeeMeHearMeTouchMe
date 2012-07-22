1f8b616a1c47a172806de7315eaa8caad974d1ac
Martin Gregory <martyg@well.com>
2012-07-21 17:45:44 -0700

    Initial Commit

---
+++ SeeMeHearMeTouchMe/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SeeMeHearMeTouchMe
+//
+//  Created by Martin Gregory on 7/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
